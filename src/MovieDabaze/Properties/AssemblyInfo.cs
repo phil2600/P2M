c562e407f682acda80d6c43609a8cf5748817815
Philippe Faure <faure.phil@gmail.com>
2010-08-11 02:37:59 +0200

    Add basical structure
    Signed-off-by: Philippe Faure <faure.phil@gmail.com>

--- src/MovieDabaze/Properties/AssemblyInfo.cs
+++ src/MovieDabaze/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("WindowsFormsApplication2")]
+[assembly: AssemblyTitle("MovieDabaze")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApplication2")]
+[assembly: AssemblyProduct("MovieDabaze")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
